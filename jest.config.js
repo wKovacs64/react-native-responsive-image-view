59e0bff9890be91a392097f46116e4994b97bce6
Justin Hall <justin.r.hall@gmail.com>
2018-11-18 19:23:11 -0700

    Fix coveragePathIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   clearMocks: true,
   coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/test'],
+  coveragePathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/setupTests.js',
+  ],
   coverageReporters: ['html', 'json', 'lcov', 'text'],
   preset: 'react-native',
   setupTestFrameworkScriptFile: '<rootDir>/setupTests.js',
