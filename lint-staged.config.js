54c847b70a39bf5ffd1634b005bbeef92d3654b1
Justin Hall <justin.r.hall@gmail.com>
2022-11-25 15:46:00 -0700

    chore: remove husky and lint-staged

--- lint-staged.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  '*.{js,jsx,ts,tsx}': ['prettier --cache --write', 'eslint --fix'],
-  '*.{html,css,json,md,mdx,yml,yaml}': ['prettier --cache --write'],
-  'README.md': ['doctoc', 'prettier --cache --write'],
-};
