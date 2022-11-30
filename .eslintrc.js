15d6a3ae7ba7b29f6ce95c9df33adf5897f5b1bf
Justin Hall <justin.r.hall@gmail.com>
2022-11-30 14:16:24 -0700

    chore: update config for TS type checking lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,4 +5,16 @@ module.exports = {
     'plugin:wkovacs64/testing-library',
     'prettier',
   ],
+  parserOptions: {
+    project: 'tsconfig.json',
+  },
+  overrides: [
+    {
+      files: ['**/*.test.ts'],
+      rules: {
+        '@typescript-eslint/unbound-method': 'off',
+        'jest/unbound-method': 'error',
+      },
+    },
+  ],
 };
